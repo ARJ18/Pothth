4e88be61f23d716a7157933abf791a2a113631d7
EyCiYo <alexchdn12@gmail.com>
2024-05-15 18:13:45 +0530

    Cards being displayed properly

--- Components/Card.gd
+++ Components/Card.gd
@@ -1,17 +1,22 @@
 extends Node2D
 
 var display_cover = true
-var texture_name
+var value
 
-func _init(name):
-	texture_name = name
+func set_properties(name,display):
+	value = name
+	display_cover = display
+	if(display):
+		var texture_val = load("res://ImageResources/PNG-cards-1.3/back_cover.png")
+		$CardTexture.texture = texture_val
+	else:
+		var texture_val = load("res://ImageResources/PNG-cards-1.3/"+name+".png")
+		$CardTexture.texture = texture_val
+	
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var texture_val = "res://ImageResources/PNG-cards-1.3/"+texture_name+".png"
-	$TextureRect.texture = texture_val
-
-
+	pass
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
